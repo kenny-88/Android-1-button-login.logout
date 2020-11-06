98c65605a815d502d5aaedf9a6f9f5aec5e85370
kenny-88 <71814199+kenny-88@users.noreply.github.com>
2020-11-06 15:02:38 +0800

    Android-1-button-login.logout

---
+++ Android1buttonloginlogout/app/src/main/java/com/example/android_1_button_loginlogout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android_1_button_loginlogout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button switch_text = (Button)findViewById(R.id.button);
+        switch_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch_text.setText("Logout");
+            }
+        });
+    }
+}
